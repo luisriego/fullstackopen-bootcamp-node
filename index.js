d1631121a49b562e8f1c21a5564ce1c1ba40c4d9
luisriego <luisriego@hotmail.com>
2021-03-19 18:51:51 -0300

    let´s install morgan

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
 const express = require('express')
 const app = express()
 
-app.use(express.json)
+app.use(express.json())
 
 let persons = [
     {
-    "name": "Arto Hellas",
-    "number": "040-123456",
-    "id": 1
+        'name': 'Arto Hellas',
+        'number': '040-123456',
+        'id': 1
     },
     {
-    "name": "Ada Lovelace",
-    "number": "39-44-5323523",
-    "id": 2
+        'name': 'Ada Lovelace',
+        'number': '39-44-5323523',
+        'id': 2
     },
     {
-    "name": "Dan Abramov",
-    "number": "12-43-234345",
-    "id": 3
+        'name': 'Dan Abramov',
+        'number': '12-43-234345',
+        'id': 3
     },
     {
-    "name": "Mary Poppendieck",
-    "number": "39-23-6423122",
-    "id": 4
+        'name': 'Mary Poppendieck',
+        'number': '39-23-6423122',
+        'id': 4
     }
 ]
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hello World</h1>')
+app.get('/api/info', (req, res) => {
+    res.send(`<p>Phonebook has info for ${persons.length} persons</p><p>${new Date().toISOString()}</p>`)
 })
 
 app.get('/api/persons', (req, res) => {
@@ -35,13 +35,12 @@ app.get('/api/persons', (req, res) => {
 })
 
 app.get('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id) 
-    person = persons.find(person => person.id === id)
-    if (person) {
-        res.json(person)
-    } else {
-        res.status(404).end()
+    const id = Number(req.params.id)
+    const person = persons.find(person => person.id === id)
+    if (!person) {
+        return res.status(404).end()
     }
+    return res.json(person)
 })
 
 app.delete('/api/persons/:id', (req, res) => {
@@ -54,7 +53,19 @@ app.post('/api/persons', (req, res) => {
     const person = req.body
     if (!person || !person.name) {
         return res.status(400).json({
-            error: `No name found in this person, found: ${person}`
+            error: 'No name found in this person!'
+        })
+    }
+
+    if (!person || !person.number) {
+        return res.status(400).json({
+            error: 'No number found in this person!'
+        })
+    }
+
+    if (persons.find(person => person.name)) {
+        return res.status(400).json({
+            error: 'Name must be unique'
         })
     }
 
@@ -69,7 +80,7 @@ app.post('/api/persons', (req, res) => {
 
     persons = [... persons, newPerson]
 
-    res.json(newPerson)
+    res.status(201).json(newPerson)
 })
 
 
