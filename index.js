023567a9a7c1db6ea949cd4e271a6d05cbb6712b
luisriego <luisriego@hotmail.com>
2021-03-20 12:03:34 -0300

    for Heroku deploy

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const cors = require('cors')
 
 app.use(express.json())
-morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
+app.use(cors())
+morgan.token('body', function (req) { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let persons = [
