3b785b890d3d513b1f1137ee50c5fab98565082d
luisriego <luisriego@hotmail.com>
2021-03-20 21:55:46 -0300

    build app

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const morgan = require('morgan')
 const cors = require('cors')
 
 app.use(express.json())
+app.use(express.static('build'))
 app.use(cors())
 morgan.token('body', function (req) { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
