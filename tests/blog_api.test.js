9bad83115315d24f4b0f2fde76e42dd4d893f37c
luisriego <luisriego@hotmail.com>
2021-04-01 23:27:26 -0300

    Exercices 4.8-4.12 completed

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -28,10 +28,57 @@ test('there are listOfBlogs.length blogs', async () => {
   expect(response.body).toHaveLength(listOfBlogs.length)
 })
 
+test('there are a UID and is named id', async () => {
+  const response = await api.get('/api/blogs')
+  const { body: blogs } = response
+  const firstBlog = blogs[0]
+  expect(firstBlog.id).toBeDefined()
+})
+
 test('there are a blogs about Go To Statement Considered Harmful', async () => {
   const response = await api.get('/api/blogs')
   const contents = response.body.map(blog => blog.title)
   expect(contents).toContain('Go To Statement Considered Harmful')
+})
+
+test('a new post can be added', async () => {
+  const newPost = {
+    title: 'A new note added',
+    author: 'José L. Riego',
+    url: 'http://www.expresate.com.br',
+    likes: 0
+  }
+  await api
+    .post('/api/blogs')
+    .send(newPost)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const response = await api.get('/api/blogs')
+  expect(response.body).toHaveLength(listOfBlogs.length + 1)
+  const contents = response.body.map(blog => blog.title)
+  expect(contents).toContain('A new note added')
+  const { body: blogs } = response
+  const lastBlog = blogs[blogs.length -1]
+  expect(lastBlog.likes).toBeDefined()
+  expect(lastBlog.likes).toBe(0)
+})
+
+test('a new post cannot be added without title and url', async () => {
+  const newPost = {
+    title: '',
+    author: 'José L. Riego',
+    url: '',
+    likes: 0
+  }
+  await api
+    .post('/api/blogs')
+    .send(newPost)
+    .expect(400)
+    .expect('Content-Type', /application\/json/)
+
+  const response = await api.get('/api/blogs')
+  expect(response.body).toHaveLength(listOfBlogs.length)
 })
 
 afterAll(() => {
